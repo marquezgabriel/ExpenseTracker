4bec2a9e4c4e8ea9a6331cdf20f55abcd272604b
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2024-06-19 13:56:22 -0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Gabriel Marquez on 2024-06-19.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
